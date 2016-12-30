6c5bcd2c0044dcb1aeff251343f2199a3c5662e1
PC-27\PC-26 <java00405@gmail.com>
2016-12-30 13:10:09 +0800

    123

---
+++ src/ex01/DateTest.java
@@ -0,0 +1,13 @@
+package ex01;
+
+import java.util.Date;
+
+public class DateTest {
+
+	public static void main(String[] args) {
+	
+        Date d = new Date();
+        System.out.println(d);
+	}
+
+}
